1e6c52819afd071b1915d9f912383fea7e7ace82
kobakichi <fenderstrat57vin@gmail.com>
2023-04-24 05:01:56 +0900

    fix サブネットのcountをfor_each関数を使用するように変更

--- main.tf
+++ main.tf
@@ -4,9 +4,24 @@ resource "aws_vpc" "tf_pr3_vpc" {
 
 }
 
-# Subnetの作成
+# countを使用してのSubnetの作成
+# resource "aws_subnet" "tf_pr3_subnet" {
+#   # length関数で、変数subnet_cidrsの長さ、個数を取得している。
+#   # この場合、リスト型で2つ存在するため、countでは2回回数を指定していることになる。
+#   # countは
+#   count      = length(var.subnet_cidrs)
+#   vpc_id     = aws_vpc.tf_pr3_vpc.id
+#   cidr_block = var.subnet_cidrs[count.index]
+# }
+
+# for_eachを使用してのサブネット作成
+
 resource "aws_subnet" "tf_pr3_subnet" {
-  count      = length(var.subnet_cidrs)
-  vpc_id     = aws_vpc.tf_pr3_vpc.id
-  cidr_block = var.subnet_cidrs[count.index]
+  for_each = toset(var.subnet_cidrs)
+  vpc_id = aws_vpc.tf_pr3_vpc.id
+  cidr_block = each.value
+
+  tags = {
+    Name = "practice3-${each.key}"
+  }
 }
