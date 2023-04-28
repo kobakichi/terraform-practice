e480105386877b4f77d87e72f0d73285a0c3a6d7
kobakichi <fenderstrat57vin@gmail.com>
2023-04-29 05:22:43 +0900

    add outputにfor文を追加。指定した値のみを表示させるようにした

--- output.tf
+++ output.tf
@@ -5,3 +5,10 @@
 output "security_group_id" {
   value = aws_security_group.tf_pr3_sg.id
 }
+
+# subnetの情報から、subnetIDだけ取得したい場合の処理にはforを使用する
+# value.の後ろを変更することで、それぞれの値のみを表示させることができる
+output "subnet_information" {
+  value = [ for value in aws_subnet.tf_pr3_subnet : value.arn]
+  # value = aws_subnet.tf_pr3_subnet
+}
