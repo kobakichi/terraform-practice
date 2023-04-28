003e4ec75eb8f34fac30a4b53b7c237b3488aa1d
kobakichi <fenderstrat57vin@gmail.com>
2023-04-29 04:42:36 +0900

    add variables.tfにmap/object型を定義。この変数をmain.tfで参照するようにした。

--- variables.tf
+++ variables.tf
@@ -9,7 +9,15 @@ variable "subnet_cidrs" {
   description = "サブネットのCIDR"
 }
 
-variable "sg_allow_cidrs" {
-  type        = list(string)
-  description = "SecurityGroupのAllowCIDRリスト"
+# 以下は3-4で削除した想定
+# variable "sg_allow_cidrs" {
+#   type        = list(string)
+#   description = "SecurityGroupのAllowCIDRリスト"
+# }
+
+# map/object型を定義
+variable "sg_ingress_rules" {
+  type = map(object({
+    protocol = string
+  }))
 }
